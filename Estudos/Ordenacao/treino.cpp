bad491d56f6a7c9adbee1a26676ff3c5117045d9
matheus4amaral <matheusamaral101204@gmail.com>
2025-10-15 08:19:33 -0300

    Add outros sorts

--- Estudos/Ordenacao/treino.cpp
+++ Estudos/Ordenacao/treino.cpp
@@ -23,7 +23,8 @@ int main() {
     exibir(v);
     cout << endl;
 
-    bubbleSort(v);
+    //quickSort(v, 0, v.size());
+    insertionSort(v);
     exibir(v);
     cout << endl;
 
@@ -51,11 +52,30 @@ void exibir(vector<int> &v) {
 }
 
 void insertionSort(vector<int> &v) {
-
+    for(int i = 1; i < v.size(); i++) {
+        int chave = v[i], j = i-1;
+        while(j >= 0 && v[j] > chave) {
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = chave;
+    }
 }
 
 int particionamento(vector<int> &v, int e, int d) {
+    int p = e, k, atual;
+    k = atual = e + 1;
 
+    while(atual <= d) {
+        if(v[atual] <= v[p]) {
+            swap(v[atual], v[k]);
+            k++;
+        }
+        atual++;
+    }
+    swap(v[k-1], v[p]);
+
+    return k - 1;
 }
 
 void preencher(vector<int> &v, int min, int max) {
@@ -64,9 +84,20 @@ void preencher(vector<int> &v, int min, int max) {
 }
 
 void quickSort(vector<int> &v, int e, int d) {
+    if(e >= d)
+        return;
     
+    int p = particionamento(v, e, d);
+    quickSort(v, e, p-1);
+    quickSort(v, p+1, d);
 }
 
 void selectionSort(vector<int> &v) {
-
+    for(int i = 0; i < v.size()-1; i++) {
+        int idMenor = i;
+        for(int j = i+1; j < v.size(); j++) 
+            if(v[idMenor] > v[j])
+                idMenor = j;
+        swap(v[i], v[idMenor]);
+    }
 }
